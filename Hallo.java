830d330ba0dd3de0562d9070e45f813ed53ae915
Marcel Schneider <m.schneider.neuffen@gmail.com>
2011-11-01 17:17:15 +0100

    run evrything.

---
+++ Hallo.java
@@ -0,0 +1,8 @@
+/*
+ * Run: java Hallo MArcel
+ */ 
+public class Hallo {
+  public static void main(String[] args) {
+    System.out.println("Hallo " + args[0] + "! -- from Java");
+  }
+}
---
+++ fak.cpp
@@ -0,0 +1,18 @@
+/*
+ * Compile: g++ -lcln fak.cpp -o fak
+ */
+#include <cln/integer.h>//for mathematical operations on arbitrarily long integers
+#include <cln/integer_io.h>//for input/output of long integers
+#include <iostream>
+
+cln::cl_I fakultaet(int i) {
+  if(i == 1) return 1;
+  return fakultaet(i - 1) * i;
+}
+ 
+int main( ) {
+  int n = 100;
+  cln::cl_I product = fakultaet(n) ;
+  std::cout << "The result of " << n << "! is " << product << " !\n" ;
+  return 0 ;
+}
